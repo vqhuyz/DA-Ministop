3430ef2e8f679b4d5e1b29cf466b962ad8e13ea9
vqhuyz <vuquochuyz99@gmail.com>
2020-10-03 10:45:29 +0700

    3/10

---
+++ Ministop/App_Start/UnityConfig.cs
@@ -0,0 +1,26 @@
+using Ministop.DI.Implements;
+using Ministop.DI.Interfaces;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace Ministop
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+            container.RegisterType<IKhachHangService, KhachHangService>();
+            container.RegisterType<INhaCungCapService, NhaCungCapService>();
+            container.RegisterType<INhanVienService, NhanVienService>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
---
+++ Ministop/ModelsView/DangNhapViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ministop.ModelsView
+{
+    public class DangNhapViewModel
+    {
+        public int NhanVienID { get; set; }
+
+        public int? PhanQuyenID { get; set; }
+
+        public string TaiKhoan { get; set; }
+
+        public string MatKhau { get; set; }
+
+        public bool? TinhTrang { get; set; }
+    }
+}
